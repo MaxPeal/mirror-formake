353cb4560942daa205ccb2eaee8164394ec5dac1
Oleksiy Chernyavskyy <ochern@rocketmail.com>
2017-01-05 20:51:37 +0200

    Formake

---
+++ src/iflib/test/test.c
@@ -0,0 +1,21 @@
+
+enum mon_type {
+  JAN=1,
+  FEB,
+  MAR
+};
+
+
+extern void *XCreateWindow;
+extern void *xmPushButtonWidgetClass;;
+
+int month = FEB;
+
+int main(int argc, char *argv[]) {
+  void *a;
+  void *b = xmPushButtonWidgetClass;
+
+
+  a= XCreateWindow;
+}
+
