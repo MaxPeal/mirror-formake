a0f269933b91eb6acfe442f86a0f9588506d6632
Oleksiy Chernyavskyy <ochern@rocketmail.com>
2017-06-03 17:48:59 +0300

    big refactoring

--- src/mkdep/test/run_test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-./formake/mkrule init >Makefile
-./formake/mkrule obj -d _obj -src *.c >>Makefile
-./formake/mkrule app -n app123 -all -install -obj _obj/file1.o _obj/file2.o >>Makefile
